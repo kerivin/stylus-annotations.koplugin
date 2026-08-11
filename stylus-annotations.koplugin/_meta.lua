9350d783b4a3a099180e0c39ca55e7e75ad0ad51
kerivin <kerivin.zero@gmail.com>
2026-08-11 21:17:17 +0300

    better description

--- stylus-annotations.koplugin/_meta.lua
+++ stylus-annotations.koplugin/_meta.lua
@@ -1,6 +1,6 @@
 local _ = require("gettext")
 return {
     fullname = _("Stylus annotations"),
-    version = "0.1.0",
-    description = _([[Annotate PDFs with freehand stylus strokes, saved in a document sidecar.]]),
+    version = "0.2.0",
+    description = _("Hand-drawn stylus annotations for PDF, EPUB and FB2 documents on Android and Linux."),
 }
