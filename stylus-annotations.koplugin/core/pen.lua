129098eeedeb6f00ca12717306cc8a76e50d75cd
kerivin <kerivin.zero@gmail.com>
2026-08-18 03:36:08 +0300

    fixes after cleanup

--- stylus-annotations.koplugin/core/pen.lua
+++ stylus-annotations.koplugin/core/pen.lua
@@ -1,17 +1,11 @@
 local Device = require("device")
 local Geometry = require("core/geometry")
 local logger = require("logger")
-local PenFallback = require("core/pen_fallback")
 
 local TOOL_TYPE_PEN = Device.input.TOOL_TYPE_PEN
 local TOOL_TYPE_ERASER = Device.input.TOOL_TYPE_ERASER
 local TOOL_TYPE_HIGHLIGHTER = Device.input.TOOL_TYPE_HIGHLIGHTER
 local TOOL_TYPE_FINGER = Device.input.TOOL_TYPE_FINGER
-
-local FULL_SCREEN_ZONE = {
-    ratio_x = 0, ratio_y = 0,
-    ratio_w = 1, ratio_h = 1,
-}
 
 local PenInput = {}
 
@@ -21,7 +15,6 @@ local gesture_hook_added = false
 function PenInput:new(plugin)
     local o = {
         plugin = plugin,
-        stylus_registered = false,
         pen_active = false,
         pen_lift_pending = false,
         pen_lift_x = 0,
@@ -29,7 +22,6 @@ function PenInput:new(plugin)
         lift_match_radius = 20,
         pen_mirrored = false,
         eraser_active = false,
-        fallback = nil,
     }
     return setmetatable(o, { __index = PenInput })
 end
@@ -49,10 +41,6 @@ end
 
 function PenInput:isPenActive()
     return self.pen_active
-end
-
-function PenInput:isPenInputAllowed()
-    return self:isPenActive() and not self.plugin:isOverlayActive() and self.plugin:isEnabled()
 end
 
 function PenInput:installGestureHook()
@@ -81,48 +69,14 @@ end
 function PenInput:register()
     self:installGestureHook()
     PenInput.current = self
-    self:registerPanZones()
-    self:installFallbackHook()
     self:registerPlatformInput()
 end
 
 function PenInput:unregister()
     self:unregisterPlatformInput()
-    self:cleanupFallbackHook()
     if PenInput.current == self then
         PenInput.current = nil
     end
-end
-
-function PenInput:registerPanZones()
-    self.plugin.ui:registerTouchZones({
-        {
-            id = "stylus_annotations_pen_pan",
-            ges = "pan",
-            screen_zone = FULL_SCREEN_ZONE,
-            overrides = {
-                "rolling_pan",
-                "rolling_pan_release",
-                "paging_pan",
-                "paging_pan_release",
-            },
-            handler = function(ges)
-                return self:onPenPan(ges)
-            end,
-        },
-        {
-            id = "stylus_annotations_pen_pan_release",
-            ges = "pan_release",
-            screen_zone = FULL_SCREEN_ZONE,
-            overrides = {
-                "rolling_pan_release",
-                "paging_pan_release",
-            },
-            handler = function(ges)
-                return self:onPenPanRelease(ges)
-            end,
-        },
-    })
 end
 
 function PenInput:registerPlatformInput()
@@ -131,7 +85,6 @@ function PenInput:registerPlatformInput()
         Input:registerStylusCallback(function(input, slot)
             return self:onStylusEvent(input, slot)
         end)
-        self.stylus_registered = true
     else
         logger.warn("PenInput: stylus callback API not available")
     end
@@ -141,22 +94,6 @@ function PenInput:unregisterPlatformInput()
     local Input = Device.input
     if Input and Input.unregisterStylusCallback then
         Input:unregisterStylusCallback()
-    end
-    self.stylus_registered = false
-end
-
-function PenInput:installFallbackHook()
-    if self.fallback then return end
-    local Input = Device.input
-    if not Input or not Input.gesture_detector then return end
-    self.fallback = PenFallback:new(self)
-    self.fallback:install()
-end
-
-function PenInput:cleanupFallbackHook()
-    if self.fallback then
-        self.fallback:cleanup()
-        self.fallback = nil
     end
 end
 
@@ -187,7 +124,7 @@ function PenInput:onStylusEvent(input, slot)
     logger.dbg("PenInput:onStylusEvent",
         "slot=", slot.slot, "tool=", slot.tool, "id=", slot.id,
         "raw=", slot.x, slot.y, "pos=", x, y,
-        "eraser=", self.eraser_active)
+        "eraser=", self.eraser_active, "mirrored=", self.pen_mirrored)
 
     if slot.tool == TOOL_TYPE_FINGER
         and input.pen_slot and slot.slot == input.pen_slot then
@@ -207,15 +144,10 @@ function PenInput:onStylusEvent(input, slot)
     end
 
     if slot.id and slot.id >= 0 then
-        if self.pen_mirrored and plugin:isOverlayActive() then
-            return true
-        end
         if not self.pen_active and plugin:isOverlayActive() then return false end
         if not plugin:isEnabled() then return false end
         self.pen_lift_pending = false
-        if not self.pen_active then
-            self.pen_active = true
-        end
+        self.pen_active = true
 
         if plugin.current_stroke then
             plugin:addStrokePoint(x, y)
@@ -233,10 +165,6 @@ function PenInput:onStylusEvent(input, slot)
             if was_active then
                 self.pen_lift_pending = true
             end
-        end
-
-        if self.pen_mirrored and plugin:isOverlayActive() then
-            return true
         end
 
         if not plugin:isOverlayActive() and plugin:isEnabled() and was_active then
@@ -257,38 +185,6 @@ function PenInput:onEraserEvent(x, y, id)
         self.eraser_active = false
         return true
     end
-end
-
-function PenInput:onPenPan(ges)
-    local plugin = self.plugin
-    if plugin.current_stroke then
-        if not self.stylus_registered then
-            plugin:addStrokePoint(ges.pos.x, ges.pos.y)
-        end
-        return true
-    end
-
-    if not self:isPenInputAllowed() then return false end
-    if self.stylus_registered then return true end
-    local start = ges.start_pos or ges.pos
-    plugin:startStroke(start.x, start.y)
-    if plugin.current_stroke then
-        plugin:addStrokePoint(ges.pos.x, ges.pos.y)
-    end
-    return true
-end
-
-function PenInput:onPenPanRelease(ges)
-    local plugin = self.plugin
-    if plugin.current_stroke then
-        if not self.stylus_registered then
-            plugin:endStroke()
-        end
-        return true
-    end
-
-    if not self:isPenInputAllowed() then return false end
-    return true
 end
 
 return PenInput
